ddd161e9d5f037b244e404a8c031ae03cdf0bf11
Filipe Costa <filipebnks@gmail.com>
2020-09-08 14:45:58 -0300

    updated files

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,11 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config.from_object('config')
 
-login_manager = LoginManager(app)
-
 
 tables.initialize_pg_db() #cria as tables
+
+lm = LoginManager()
+lm.init_app(app)
 
 from app.models import tables, forms
 from app.controllers import default
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,16 +1,16 @@
 #Aqui ficam as rotas das páginas
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user
-from app import app, login_manager
+from app import app, lm
 from app.models.tables import db
 
 from app.models.tables import Users
 from app.models.forms import LoginForm
 
 
-@login_manager.user_loader
+@lm.user_loader
 def load_user(id):
-    return Users.select().where(Users.id==id)
+    return Users.select().where(Users.id==id).first()
 
 @app.route("/index")
 @app.route("/")
@@ -25,7 +25,7 @@ def login():
         # print(user.password)
         # print(form.password)
         if user and user.password == form.password.data:
-            print(login_user(user))
+            login_user(user)
             flash("Logged in.")
             return redirect(url_for("index"))
             # print(form.username.data)
@@ -42,12 +42,13 @@ def logout():
     return redirect(url_for("index"))
 
 
+
+# essa parte pode ter funcionalidade de criar usuário
 # @app.route("/teste/<info>")
 # @app.route("/teste", defaults={"info": None})
 # def teste(info):
-#     i = Users.create(username='filipecosta', name='Filipe', password='m249sopmod', email='filipi@gmail.com')
+#     i = Users.create(username='emily', name='Emily Costa', password='1234', email='emily@gmail.com')
 #     # i = Users.delete().where(Users.name== 'hig').execute()
 #     i.save()
 #     return "ok"
 
-# 'ModelObjectCursorWrapper' object has no attribute 'password'
